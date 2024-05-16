69a294a9902025a3c2976829fc966c3fa7320f35
Yose Rafhael <yrafhael@my.wctc.edu>
2024-05-16 10:56:11 -0500

    [1.01] Initial commit I use previous set up from project that has been done in the lab activities.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using NLog;
+using Final_project.Model;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
